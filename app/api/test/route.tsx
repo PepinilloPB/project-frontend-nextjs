cc7a3a56ba334f0ad14a0f718373f2bfeca47651
PepinilloPB <eddycarrascoclaros@gmail.com>
2023-11-22 16:26:09 -0400

    API local removida

--- app/api/test/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const GET = async (req: any, res: any) =>{
-    return NextResponse.json({ message: "OK" }, { status: 200 });
-}
-
-export const POST = async (req: Request, res: Response) =>{
-    console.log("Hello World")
-}
